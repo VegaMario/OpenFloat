dbb8d69768192514e4b2944a12a7d51820a7fee2
VegaMario <mariovegajr00@gmail.com>
2025-08-18 15:26:56 -0500

    cleaning up

--- test_run_dir/DUT_should_do_something/verilated/VFP_add_32_7__ALLcls.d
+++
@@ -1,5 +0,0 @@
-VFP_add_32_7__ALLcls.o: VFP_add_32_7__ALLcls.cpp VFP_add_32_7.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_cov.h VFP_add_32_7.cpp \
- VFP_add_32_7.h VFP_add_32_7__Syms.h
--- test_run_dir/DUT_should_do_something/verilated/VFP_add_32_7__ALLsup.d
+++
@@ -1,7 +0,0 @@
-VFP_add_32_7__ALLsup.o: VFP_add_32_7__ALLsup.cpp VFP_add_32_7.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_cov.h VFP_add_32_7__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h VFP_add_32_7__Syms.h \
- VFP_add_32_7.h VFP_add_32_7__Syms.cpp VFP_add_32_7__Trace__Slow.cpp
